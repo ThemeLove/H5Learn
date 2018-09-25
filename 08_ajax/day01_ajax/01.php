8eece5cede46917bebd72de8f5545625285dbce2
ThemeLove <lqsthemelove@163.com>
2018-09-26 01:26:07 +0800

    add 08_ajax/

---
+++ 08_ajax/day01_ajax/01.php
@@ -0,0 +1,21 @@
+<?php
+//设置编码格式为utf-8
+header('content-type:text/html;charset=utf-8');
+//单行注释
+/*
+    多行注释
+*/
+//PHP中定义变量通通都是$开头
+$num=1;//整数
+$float=3.1415926;//浮点数
+$str='php是世界上最好的语言';//字符串
+$bool=false;//布尔
+//输出语句为echo
+echo 'hello';
+echo '<br>';
+echo 'php';
+echo '<br>';
+echo 'the best language in the world!';
+echo '<br>';
+echo '<input type="button" value="点我试试">'
+?>
