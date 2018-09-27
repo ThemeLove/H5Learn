92ca094e28c834375af8c2b31fff930c51f6738c
ThemeLove <lqsthemelove@163.com>
2018-09-27 12:49:46 +0800

    modify

---
+++ 08_ajax/day01_ajax/05get.php
@@ -0,0 +1,6 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+echo 'get请求';
+echo '<br>';
+echo $_GET['username'].' 欢迎回来';
+?>
---
+++ 08_ajax/day01_ajax/05post.php
@@ -0,0 +1,6 @@
+<?php
+header('content-type:text/html; charset= utf-8');
+echo 'post请求';
+echo '<br>';
+echo $_POST['username'].'欢迎回来';
+?>
