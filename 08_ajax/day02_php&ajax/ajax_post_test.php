ebff5ea59b2ce3392b2d9277568eba2372cd9cad
ThemeLove <lqsthemelove@163.com>
2018-09-28 20:25:40 +0800

    modify

---
+++ 08_ajax/day02_php&ajax/ajax_post_test.php
@@ -0,0 +1,27 @@
+<?php
+header('content-type:application/json; charset= utf-8');
+    $dataArr=array(
+            '1'=>array('name'=>'jack','age'=>'18','skill'=>'帅'),
+            '2'=>array('name'=>'rose','age'=>'17','skill'=>'美'),
+            '3'=>array('name'=>'iceMountain','age'=>'10000','skill'=>'hitBoat')
+    );
+    $id=$_POST['id'];
+    $data=NULL;
+    if(array_key_exists($id,$dataArr)){//判断该key是否在关系型数组中
+           $data=$dataArr[$id];
+    }
+
+    $resultArr=NULL;
+    if(!empty($data)){
+        $resultArr=array('code'=>"0",
+                          'message'=>"success",
+                           'result'=>$data
+                           );
+    }else{
+        $resultArr=array('code'=>'1',
+                        'message'=>'fail',
+                        'result'=>''
+                        );
+    }
+    exit(json_encode($resultArr));
+?>
