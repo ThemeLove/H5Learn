a2814ae246f061c0db5a1bb7639a4a0f8d7b2083
ThemeLove <lqsthemelove@163.com>
2018-10-10 18:56:49 +0800

    modify

--- 08_ajax/day05_ajax/waterFall/js/jquery.waterFall.js
+++ 08_ajax/day05_ajax/waterFall/js/jquery.waterFall.js
@@ -1,49 +1,69 @@
 jQuery.fn.extend({
     waterFall:function (option) {
         console.log("我是自定义插件方法：wallFall");
-        var items=this.children();
-        console.log("count="+items.length);
-    //  获取最后五个元素，因为这里我们一排最多放5个
-        var tempItemArr=[items.eq(-1),items.eq(-2),items.eq(-3),items[-4],items[-5]];
-        console.log("eq(1)top="+items.eq(1).position().top+",eq(1)left="+items.eq(1).position().left);
-        console.log("eq(-2)="+items.eq(-2).children("p").eq(0).text());
-        console.log("eq(-3)="+items.eq(-3).width()+",height="+items.eq(-3).height());
+    //    获取父控件的总宽度
+        var totalWidth=this.width();
+    //    获取每个item的宽度
+        var items=this.children(".item");
+        var itemWidth=items.width();
+        console.log("totalWidth="+totalWidth);
+        console.log("itemWidth="+itemWidth);
 
+    //    根据totalWidth和itemWidth计算每行可以容纳的列数
+        var columnNum=Math.floor(totalWidth/itemWidth);
+    //    计算每行剩余的宽度
+        var margin=Math.floor((totalWidth-itemWidth*columnNum)/(columnNum+1));
 
-        var tempFiveItemArr=[
-            {
-                left:items.eq(-1).position().left,
-                top:items.eq(-1).position().top+items.eq(-1).height()
-            },
-            {
-                left:items.eq(-2).position().left,
-                top:items.eq(-2).position().top+items.eq(-2).height()
-            },
-            {
-                left:items.eq(-3).position().left,
-                top:items.eq(-3).position().top+items.eq(-3).height()
-            },
-            {
-                left:items.eq(-4).position().left,
-                top:items.eq(-4).position().top+items.eq(-4).height()
-            },
-            {
-                left:items.eq(-5).position().left,
-                top:items.eq(-5).position().top+items.eq(-5).height()
+        console.log("columnNum="+columnNum);
+        console.log("margin="+margin);
+
+    //    定义一个空数组，用来保存最后一行的高度,并初始化为margin(可以为其他值，影响第一行的上边距)
+        var highArr=[];
+        //初始化
+        for(var i=0;i<columnNum;i++){
+            highArr[i]=margin;
+        }
+
+    //    循环每个item，动态的改变他们的left和top值，实现瀑布流效果。
+    //    步骤1：循环highArr数组，找出最小值和对应索引
+    //    步骤2：根据最小值，计算出item应当放置的位置，设置item的left和top值
+    //    步骤3：根据item的位置，更新highArr数组
+    //    步骤4：每个item位置设置完毕后，找出highArr中的最大值，设置父布局的高度
+        var minHigh=margin;
+        var minIndex=0;
+        items.each(function (index, item) {//注意这里的item 是DOM中的对象，要用$(item)包装一下才能用jq中的方法
+            //找出highArr中的最小值和索引
+            for(var i=0;i<highArr.length;i++){
+                if(highArr[i]<minHigh){
+                    minHigh=highArr[i];
+                    minIndex=i;
+                }
             }
-        ];
-
-        var orderFiveItemArr=tempFiveItemArr.sort(function (obj1,obj2) {
-            if(Number(obj1.top)<Number(obj2.top)){
-                return -1;
-            }else if(Number(obj1.top)>Number(obj2.top)){
-                return 1
-            }else{
-                return 0;
-            }
+            //根据最小值，计算出item应当放置的位置，设置item的left和top值
+            var left=$(item).width()*minIndex+margin*(minIndex+1);
+            var top=minHigh;
+            console.log("highArr="+highArr);
+            console.log("left="+left);
+            console.log("top="+top);
+            $(item).css({
+                "left":left,
+                "top":top,
+                "padding-bottom":"10px"
+            });
+            // 根据item的位置，更新highArr数组
+            minHigh+=$(item).height()+margin+10;
+            highArr[minIndex]=minHigh;
+            console.log("itemWidth="+$(item).width());
+            console.log("itemHeight="+$(item).height());
         });
 
-
-
+        //    找到highArr中的最大值，设置给items ，这样动态改变items 的高度，将加载跟多按钮顶到最下面去
+        var maxHigh=0;
+        for(var i=0;i<highArr.length;i++){
+            if (highArr[i]>maxHigh){
+                maxHigh=highArr[i];
+            }
+        }
+        this.height(maxHigh);
     }
 });
