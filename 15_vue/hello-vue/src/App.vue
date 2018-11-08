913e2d2d4f4b4b02bb453788e9573416c5d7be57
ThemeLove <lqsthemelove@163.com>
2018-11-09 01:53:29 +0800

    modify

--- 15_vue/hello-vue/src/App.vue
+++ 15_vue/hello-vue/src/App.vue
@@ -1,22 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNewItem">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggle(item)">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Store from './store.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data:function(){
+    return{
+      title: 'this is a todo list',
+      items:Store.fetch(),
+      newItem:""
+    }
+  },
+  methods:{
+    toggle:function (item) {
+      item.isFinished=!item.isFinished;
+    },
+    addNewItem:function () {
+
+      this.items.push(
+        {
+          label:this.newItem,
+          isFinished:false
+        }
+      );
+
+      this.newItem="";
+    }
+  },
+  watch:{
+    items:{
+      handler:function (items) {
+          Store.save(items)
+      },
+      deep:true
+    }
   }
 }
 </script>
 
 <style>
+  .finished{
+    text-decoration: line-through;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
