1a6e42b693e6a9e07cbe317b911cad6509471d1d
ThemeLove <lqsthemelove@163.com>
2018-11-21 18:22:15 +0800

    modify

--- 15_vue/sell/src/components/food/food.vue
+++ 15_vue/sell/src/components/food/food.vue
@@ -17,13 +17,23 @@
             <div class="price">
               <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
             </div>
-          </div>
-          <div class="cartcontrol-wrapper">
+            <div class="cartcontrol-wrapper">
               <cartcontrol :food="food" v-on:add-cart="addCart"></cartcontrol>
+            </div>
+            <transition name="fade">
+              <div class="buy" v-show="!food.count||food.count===0" @click="addFirstFood($event)">加入购物车</div>
+            </transition>
           </div>
-          <transition name="fade">
-            <div class="buy" v-show="!food.count||food.count===0" @click="addFirstFood($event)">加入购物车</div>
-          </transition>
+          <split v-show="food.info"></split>
+          <div class="info" v-show="food.info">
+            <h1 class="title">商品信息</h1>
+            <p class="text">{{food.info}}</p>
+          </div>
+          <split></split>
+          <div class="rating">
+            <h1 class="title">商品评价</h1>
+            <ratingselect v-bind:select-type="selectType" v-bind:only-content="onlyContent" v-bind:desc="desc" v-bind:ratings="food.ratings"></ratingselect>
+          </div>
         </div>
       </div>
     </transition>
@@ -33,6 +43,12 @@
   import BScroll from "better-scroll"
   import cartcontrol from "components/cartcontrol/cartcontrol"
   import Vue from "vue"
+  import split from "components/split/split"
+  import ratingselect from "components/ratingselect/ratingselect"
+
+  const POSITIVE =0 ;
+  const NEGATIVE =1 ;
+  const ALL =2 ;
 
     export default {
         name: "food",
@@ -42,16 +58,27 @@
           }
         },
         components:{
-          "cartcontrol":cartcontrol
+          "cartcontrol":cartcontrol,
+          "split":split,
+           "ratingselect":ratingselect
         },
         data (){
           return {
-            showFlag:false
+            showFlag:false,
+            selectType:ALL,
+            onlyContent:true,
+            desc:{
+              all:'全部',
+              positive:"推荐",
+              negative:"吐槽"
+            }
           }
         },
         methods :{
           show(){
             this.showFlag=true;
+            this.selectType = ALL;
+            this.onlyContent = true;
             this.$nextTick( () =>{
               if(!this.scroll){
                 this.scroll=new BScroll(this.$refs.food,{
@@ -117,9 +144,8 @@
           padding: 10px
           font-size: 20px
           color: #fff
-
-
     .content
+      position: relative
       padding:18px
       .title
         line-height: 14px
@@ -148,25 +174,45 @@
           text-decoration: line-through
           font-size: 10px
           color: rgb(147,153,159)
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding:0 12px
+        box-sizing:border-box
+        font-size:10px
+        border-radius:12px
+        color: #fff
+        background-color: rgb(0,160,220)
+        transition:all 0.5s linear
+        &.fade-enter,&.fade-leave-to
+          opacity:0
 
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding:0 12px
-      box-sizing:border-box
-      font-size:10px
-      border-radius:12px
-      color: #fff
-      background-color: rgb(0,160,220)
-      transition:all 0.5s linear
-      &.fade-enter,&.fade-leave-to
-        opacity:0
+    .info
+      padding:18px
+      .title
+        line-height:14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding:0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
+    .rating
+      padding-top:18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7,17,27)
+
 </style>
---
+++ 15_vue/sell/src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,116 @@
+<template>
+<div class="ratingselect">
+  <div class="rating-type border-1px">
+    <span class="block positive" v-bind:class="{'active':selectType===2}" @click="select(2,$event)">{{desc.all}}<span class="count">47</span></span>
+    <span class="block positive" v-bind:class="{'active':selectType===0}" @click="select(0,$event)">{{desc.positive}}<span class="count">47</span></span>
+    <span class="block negative" v-bind:class="{'active':selectType===1}" @click="select(1,$event)">{{desc.negative}}<span class="count">47</span></span>
+  </div>
+  <div class="switch" v-bind:class="{'on':onlyContent}">
+    <span class="icon-check_circle" ></span>
+    <span class="text">只看有内容的评价</span>
+  </div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+    const POSITIVE =0 ;
+    const NEGATIVE =1 ;
+    const ALL =2 ;
+    export default {
+        name: "ratingselect",
+        props:{
+          ratings:{
+            type:Array,
+            default (){
+              return [];
+            }
+          },
+          selectType:{
+            type:Number,
+            default:ALL
+          },
+          onlyContent:{
+            type:Boolean,
+            default:false
+          },
+          desc:{
+            type:Object,
+            default (){
+              return {
+                all:"全部",
+                positive:"满意",
+                negative:"不满意"
+              }
+            }
+          }
+        },
+        data(){
+          return {
+          }
+        },
+        methods:{
+          toggleOnlyContent (){
+
+          },
+          select (type,event){
+            console.log("select");
+            if(!event._constructed){
+              return;
+            }
+            console.log("select after");
+            this.selectType=type;
+          }
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .ratingselect
+    .rating-type
+      padding: 18px 0
+      margin: 0 18px
+      border-1px(rgba(7,17,27,0.1))
+      font-size:0
+      .block
+        display: inline-block
+        padding:8px 12px
+        margin-right:8px
+        border-radius: 1px
+        color: rgb(77,85,93)
+        font-size:12px
+        line-height: 16px
+        &.active
+          color:#fff
+        .count
+          font-size:8px
+          margin-left:2px
+        &.positive
+          background-color: rgba(0,160,220,0.2)
+          &.active
+            background-color: rgb(0,160,220)
+        &.negative
+          background-color: rgba(77,85,93,0.2)
+          &.active
+            background-color: rgb(77,85,93)
+
+
+    .switch
+      padding:12px 18px
+      font-size:0
+      line-height: 24px
+      border-bottom:1px solid rgba(7,17,27,0.1)
+      color: rgb(147,153,159)
+      &.on
+        .icon-check_circle
+          color:#00c850
+      .icon-check_circle
+        margin-right:4px
+        font-size:24px
+        display: inline-block
+        vertical-align: top
+      .text
+        font-size:12px
+        vertical-align: top
+
+</style>
